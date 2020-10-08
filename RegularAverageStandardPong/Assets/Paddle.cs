d02fc6136a0f413bad21fe7915e6a0ac54f9930f
parke1wh <71412819+parke1wh@users.noreply.github.com>
2020-10-08 12:17:28 -0400

    Left Paddle Controls

---
+++ RegularAverageStandardPong/Assets/Paddle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 position = this.transform.position;
+            if (position.z > -38.5f) { 
+                position.z -= 1f;
+                this.transform.position = position;
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 position = this.transform.position;
+            if (position.z < 38.5f) { 
+                position.z += 1f;
+                this.transform.position = position;
+            }
+        }
+    }
+}
