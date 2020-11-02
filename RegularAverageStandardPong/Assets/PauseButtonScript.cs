cd8c841f5d64817a2bf6f633bf7ea4aa3e54c43b
Zach Ferguson <zachfergus1@gmail.com>
2020-11-02 11:44:25 -0500

    #20 , #15 , #16, Animation with Win/Loss Screens
    Currently writing the script for the Pause Button settings, but the button itself is done here.

---
+++ RegularAverageStandardPong/Assets/PauseButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Code used from https://www.youtube.com/watch?v=9H-PtEtCVZM
+public class PauseButtonScript : MonoBehaviour
+{
+    bool isPaused = false;
+
+    public void pauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+        } 
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
+}
