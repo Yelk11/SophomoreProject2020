80f6378a59a10099631807c576ab8692e3652335
parke1wh <71412819+parke1wh@users.noreply.github.com>
2020-11-10 09:54:16 -0500

    Reflection

--- RegularAverageStandardPong/Assets/Scripts/Paddle.cs
+++ RegularAverageStandardPong/Assets/Scripts/Paddle.cs
@@ -34,4 +34,25 @@ public class Paddle : MonoBehaviour
             }
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float force = 0.000000000005f;
+        ContactPoint redirection = collision.GetContact(0);
+        Vector3 velocity = collision.rigidbody.velocity;
+
+        if (redirection.point.x > transform.position.x)
+        {
+            Vector3 direction = new Vector3(0, 0, 1);
+            collision.rigidbody.AddForce(direction * force, ForceMode.Impulse);
+            collision.rigidbody.AddForce(velocity / 10, ForceMode.VelocityChange);
+
+        }
+        else if (redirection.point.x < transform.position.x)
+        {
+            Vector3 direction = new Vector3(0, 0, -1);
+            collision.rigidbody.AddForce(direction * force, ForceMode.Impulse);
+            collision.rigidbody.AddForce(velocity / 10, ForceMode.VelocityChange);
+        }
+    }
 }
--- RegularAverageStandardPong/Assets/Scripts/Reflect.cs
+++ RegularAverageStandardPong/Assets/Scripts/Reflect.cs
@@ -11,8 +11,29 @@ public class Reflect : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float force = 0.000000000005f;
+        ContactPoint redirection = collision.GetContact(0);
+        Vector3 velocity = collision.rigidbody.velocity;
+
+        if (redirection.point.x > transform.position.x)
+        {
+            Vector3 direction = new Vector3(0, 0, 1);
+            collision.rigidbody.AddForce(direction * force, ForceMode.Impulse);
+            collision.rigidbody.AddForce(velocity / 10, ForceMode.VelocityChange);
+
+        }
+        else if (redirection.point.x < transform.position.x)
+        {
+            Vector3 direction = new Vector3(0, 0, -1);
+            collision.rigidbody.AddForce(direction * force, ForceMode.Impulse);
+            collision.rigidbody.AddForce(velocity / 10, ForceMode.VelocityChange);
+        }
     }
 }
