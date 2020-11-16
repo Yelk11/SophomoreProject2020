9fe873c1f0501936745b65e072aa8fd7c6f81057
parke1wh <71412819+parke1wh@users.noreply.github.com>
2020-11-16 10:38:29 -0500

    Update BallMovement.cs

--- RegularAverageStandardPong/Assets/Scripts/BallMovement.cs
+++ RegularAverageStandardPong/Assets/Scripts/BallMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BallMovement : MonoBehaviour{
     public Vector3 initialImpulse;
@@ -10,12 +11,9 @@ public class BallMovement : MonoBehaviour{
     public int p1Score = 0;
     public int p2Score = 0;
     public float speedlimit = .00000000005f;
-<<<<<<< HEAD
-=======
+
 
     private static int MAX_SCORE = 10;
-
->>>>>>> f79d22a457b3b6b8de0be48a5cbec740941faa49
     public GameObject ball;
 
     // Start is called before the first frame update
@@ -48,27 +46,26 @@ public class BallMovement : MonoBehaviour{
         v.y = 0;
         ball.GetComponent<Rigidbody>().velocity = v;
 
-<<<<<<< HEAD
-        if (GetComponent<Rigidbody>().velocity.magnitude > speedlimit)
-        {
-            GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity.normalized * speedlimit;
-=======
-
         if (GetComponent<Rigidbody>().velocity.magnitude > speedlimit)
         {
             GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity.normalized * speedlimit;
 
+            if (GetComponent<Rigidbody>().velocity.magnitude > speedlimit)
+            {
+                GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity.normalized * speedlimit;
+            }
 
 
-        // check for win
-        if (p1Score >= MAX_SCORE)
-        {
-            SceneManager.LoadScene("WinScene");
-        }
-        if(p2Score >= MAX_SCORE){
-            SceneManager.LoadScene("LossScene");
+            // check for win
+            if (p1Score >= MAX_SCORE)
+            {
+                SceneManager.LoadScene("WinScene");
+            }
+            if (p2Score >= MAX_SCORE)
+            {
+                SceneManager.LoadScene("LossScene");
 
->>>>>>> f79d22a457b3b6b8de0be48a5cbec740941faa49
+            }
         }
     }
 }
